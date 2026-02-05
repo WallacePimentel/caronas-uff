4be0db52d5710c59b2245a248fed8823a3a64e28
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-02-05 20:46:27 -0300

    feat: creating seeds to populate the database when initalizing the server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+campus = [
+          {
+            description: "Gragoatá",
+            street_adress: "Rua Prof. Marcos Waldemar de Freitas Reis",
+            number: "s/n",
+            district: "São Domingos/Gragoatá",
+            city: "Niterói",
+            CEP: "24210-201",
+            status: 0,
+            deactivation_date: nil
+          },
+          {
+            description: "Praia Vermelha",
+            street_adress: "Rua Passo da Pátria",
+            number: "152-470",
+            district: "São Domingos",
+            city: "Niterói",
+            CEP: "24210-240",
+            status: 0,
+            deactivation_date: nil
+          },
+          {
+            description: "Valonguinho",
+            street_adress: "Rua Mario Santos Braga",
+            number: "30",
+            district: "Centro",
+            city: "Niterói",
+            CEP: "24020-140",
+            status: 0,
+            deactivation_date: nil
+          }
+]
+
+campus.each do |campu|
+  Campu.find_or_create_by!(campu)
+end
