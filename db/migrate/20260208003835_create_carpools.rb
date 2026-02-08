fc05bb12023634f253040ffca80523dbc2288e53
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-02-07 21:47:06 -0300

    feat: adding table name to migration since the attribute doesn't have the table name

--- db/migrate/20260208003835_create_carpools.rb
+++ db/migrate/20260208003835_create_carpools.rb
@@ -1,8 +1,8 @@
 class CreateCarpools < ActiveRecord::Migration[6.0]
   def change
     create_table :carpools do |t|
-      t.references :begging_campus, null: false, foreign_key: true
-      t.references :ending_campus, null: false, foreign_key: true
+      t.references :beginning_campus, null: false, foreign_key: { to_table: :campus }
+      t.references :ending_campus, null: false, foreign_key: { to_table: :campus }
 
       t.timestamps
     end
