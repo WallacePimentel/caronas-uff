3c0281855a25979e2826bd41435471d9671486dc
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-02-07 23:31:12 -0300

    feat: adding relationship between Carpool model and Place model using cocoon helpers

--- app/controllers/carpools_controller.rb
+++ app/controllers/carpools_controller.rb
@@ -65,6 +65,6 @@ class CarpoolsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def carpool_params
-      params.require(:carpool).permit(:beginning_campus_id, :ending_campus_id)
+      params.require(:carpool).permit(:beginning_campus_id, :ending_campus_id, places_attributes: [:id, :street, :number, :district, :city, :CEP, :_destroy])
     end
 end
--- app/models/carpool.rb
+++ app/models/carpool.rb
@@ -1,4 +1,6 @@
 class Carpool < ApplicationRecord
   belongs_to :beginning_campus, class_name: 'Campu'
   belongs_to :ending_campus, class_name: 'Campu'
+  has_many :places, dependent: :destroy
+  accepts_nested_attributes_for :places, allow_destroy: true, reject_if: :all_blank
 end
