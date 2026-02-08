b872f3111c99bfcf835556750db940d14915aa99
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-02-07 23:30:21 -0300

    feat: schema update after creation of model Place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_02_08_003835) do
+ActiveRecord::Schema.define(version: 2026_02_08_011032) do
 
   create_table "campus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "description"
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2026_02_08_003835) do
     t.index ["ending_campus_id"], name: "index_carpools_on_ending_campus_id"
   end
 
+  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "street"
+    t.string "number"
+    t.string "district"
+    t.string "city"
+    t.string "CEP"
+    t.bigint "carpool_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["carpool_id"], name: "index_places_on_carpool_id"
+  end
+
   add_foreign_key "carpools", "campus", column: "beginning_campus_id"
   add_foreign_key "carpools", "campus", column: "ending_campus_id"
+  add_foreign_key "places", "carpools"
 end
