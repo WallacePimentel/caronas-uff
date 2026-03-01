06248f31ef83133095a1984fddf1c77743e35448
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-03-01 14:28:49 -0300

    fix: removing unnecessary comma

--- config/application.rb
+++ config/application.rb
@@ -20,12 +20,7 @@ module AtividadePraticaSti
         fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
+        routing_specs: false
     end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
