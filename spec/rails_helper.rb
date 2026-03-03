c28ab423906f849de4d1bb37f01b2e681507ce5c
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-03-03 00:40:04 -0300

    feat: adding capybara configuration to use chromium for javascript true tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,4 +73,19 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+
+  # Capybara configuration for JavaScript tests
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  Capybara.javascript_driver = :headless_chrome
+  Capybara.default_max_wait_time = 5
 end
