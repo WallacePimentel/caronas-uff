fd38c32b2433b9331bc810f4925a6b787e6a7d3a
Wallace Pimentel <wallace.r.p.jr@gmail.com>
2026-03-03 00:41:02 -0300

    fix: fixing issue where javascript assets were not being compiled to the test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,10 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
+  # Enable asset compilation for feature tests with JavaScript
+  config.assets.compile = true
+  config.assets.debug = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
